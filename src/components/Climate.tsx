51af7430be4044780f2c00a677da994b2b0a0997
Frank Blendinger <frank.blendinger@methodpark.de>
2020-11-24 20:57:14 +0100

    Add event listeners in Climate as well (useEffect)

--- src/components/Climate.tsx
+++ src/components/Climate.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Sensor } from '../lib/Sensor';
 import ClimateValue from './ClimateValue';
 
 type ClimateProps = { sensor: Sensor };
 
 function Climate({ sensor }: ClimateProps) {
+  useEffect(() => {
+    // runs at creation and on each update, but that's fine (see below)
+    console.log('registering even listeners in <Climate>');
+    sensor.on('temperature', t => console.log(`temperature: ${t}`));
+    sensor.on('humidity',    h => console.log(`humidity:    ${h}`));
+
+    // this runs when the component is unmounted
+    return () => {
+      console.log('clearing ALL event listeners (<Climate>)');
+      sensor.clearListeners();
+    }
+  }); // no deps array; then only thing that could change here is the sensor
+      // prop, and if that changes, we actually do want to unsub + sub again
+
   return (
     <>
       <ClimateValue
